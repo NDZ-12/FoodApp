67fbda5c24777e38ceb8122a07abacee3954dd77
Niranjan Zambare <niranjan.zambare@tietoevry.com>
2023-07-23 14:25:58 +0530

    Res card code commit

--- src/RestaurantCard.js
+++ src/RestaurantCard.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./RestaurantCard.css";
 
 const RestaurantCard = () => {
   return (
-    <div className='res-card'>
-        <h3>Meghana foods</h3>
+    <div className="res-card">
+    
+        <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://www.madhuseverydayindian.com/wp-content/uploads/2020/10/khichdi-instant-pot-1024x1024.jpg"></img>
+      
+      <h3> MDH Foods</h3>
+      <h4>South food ,breakfast, lunch</h4>
+      <h4>4.4 stars</h4>
+      <h4>38 minutes</h4>
+     
+      
     </div>
-  )
-}
+  );
+};
 
-export default RestaurantCard
+export default RestaurantCard;
