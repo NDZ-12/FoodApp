220a36acb138aa79065bc0909d18aa1504c95c99
Niranjan Zambare <niranjan.zambare@tietoevry.com>
2023-07-23 13:41:00 +0530

    Init Restaurant card

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import RestaurantCard from "./RestaurantCard";
 
-
-
-
-const Header =() => {
-return (
-<div className='header'>
-  <div className='logo-container'> 
-    <img className='logo' alt ='logo'src='https://dcassetcdn.com/design_img/1442230/51618/51618_7323624_1442230_c91dc92a_image.jpg' />
-
-  </div>
-  <div className='nav-items'>
-<ul>
-  <li>Home</li>
-  <li>About</li>
-  <li>Contact us</li>
-</ul>
-
-  </div>
-
-</div>
-
-)
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          alt="logo"
+          src="https://dcassetcdn.com/design_img/1442230/51618/51618_7323624_1442230_c91dc92a_image.jpg"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
-
-const AppLayout =() => {
-  return <div className='app'>
-    <Header />
+const Body =() =>
+{
+ return (
+ <div className="body">
+  <div className="search">Search</div>
+  <div className="res-conatiner">
+    <RestaurantCard />
   </div>
+ </div>
 
+ )
+
+}
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
 };
-
-
 
 export default AppLayout;
---
+++ src/RestaurantCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RestaurantCard = () => {
+  return (
+    <div className='res-card'>
+        <h3>Meghana foods</h3>
+    </div>
+  )
+}
+
+export default RestaurantCard
