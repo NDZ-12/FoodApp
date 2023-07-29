537588fe9e85ce31d01d1cf5d39127da669cd528
Niranjan Zambare <niranjan.zambare@tietoevry.com>
2023-07-29 21:06:26 +0530

    END Episode -5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,64 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import RestaurantCard from './RestaurantCard'
 import { restrautList } from '../utils/mockData'
 
 
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+
+  return filterData;
+}
+
+
+
+
+
+
 const Body =() =>
 {
+
+  const [restaurants, setRestaurants] = useState(restrautList);
+  const [searchText, setSearchText] = useState("");
+
+  function handleReset()
+  {
+   setRestaurants(restrautList);
+   setSearchText('');
+  }
  return (
  <div className="body">
-  <div className="search">Search</div>
+  <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            //need to filter the data
+            const data = filterData(searchText, restaurants);
+            // update the state - restaurants
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+        
+        <button className="reset-btn" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
+    
+  
   <div className="res-conatiner">
-   {restrautList.map((x)=>
+   {restaurants.map((x)=>
    
    
     {
