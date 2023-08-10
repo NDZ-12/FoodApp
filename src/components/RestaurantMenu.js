5d025d062e2aabfbd2d270b9969ca59101b776ed
Niranjan Zambare <niranjan.zambare@tietoevry.com>
2023-08-10 18:56:47 +0530

    Minor code change

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,28 +1,34 @@
-import useResturantMenu from '../utils/useResturantMenu';
+import useResturantMenu from "../utils/useResturantMenu";
 
-
-import Shimmer from './Shimmer';
-import { useParams } from 'react-router-dom';
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
 
 const RestaurantMenu = () => {
+  const { resId } = useParams();
+  const restInfo = useResturantMenu(resId);
+  if (restInfo === null) return <Shimmer />;
 
-const {resId}=useParams();
-  const restInfo=useResturantMenu(resId) ;
-    if (restInfo===null) return <Shimmer/>
-    
-    const {name ,cuisines,costForTwoMessage}=restInfo.cards[0].card.card.info;
-    const{ itemCards}=restInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card;
+  const { name, cuisines, costForTwoMessage } =
+    restInfo.cards[0].card.card.info;
+  const { itemCards } =
+    restInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card;
 
- 
   return (
-    <div className='menu'>
-        <h1>{name}</h1>
-        <h2>{cuisines.join(",")}- {costForTwoMessage}</h2>
-        <h2>Menu - Veg & Non veg Special </h2>
-        
-        <ul>
-            {itemCards.map((item)=> <li key={item.card.info.id}>{item.card.info.name} - {item.card.info.price/100}</li>)}
-        </ul>
+    <div className="menu">
+      <h1>{name}</h1>
+      <h2>
+        {cuisines.join(",")}- {costForTwoMessage}
+      </h2>
+      <h2>Menu - Veg & Non veg Special </h2>
+
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} - Rs{" "}
+            {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
